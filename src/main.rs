3bba3e4d4c0c75a369b3f9dc31447ebd4f0fe60d
Markus Mayer <widemeadows@gmail.com>
2024-06-08 18:35:03 +0200

    Add mapping of physical to display

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+mod projectile;
+
+use crate::projectile::Projectile;
 use ggez::glam::*;
 use ggez::graphics::{self, Canvas, Color, Rect};
 use ggez::winit::dpi::PhysicalSize;
@@ -16,85 +19,8 @@ struct MainState {
     projectile: Option<Projectile>,
     projectile_trajectory: Option<Vec<Vec2>>,
     gravity: Vec2,
-}
-
-#[derive(Clone)]
-struct Projectile {
-    position: Vec2,
-    velocity: Vec2,
-    acceleration: Vec2,
-    radius: f32,
-}
-
-impl Default for Projectile {
-    fn default() -> Self {
-        Self {
-            position: Vec2::default(),
-            velocity: Vec2::default(),
-            acceleration: Vec2::default(),
-            radius: 5.0,
-        }
-    }
-}
-
-impl Projectile {
-    pub fn fire_from(position: Vec2) -> Self {
-        // TODO: Convert from cartesian to polar, angle and force
-
-        let mut rng = rand::thread_rng();
-        let random_x: f32 = rng.gen_range(-10.0..=10.0);
-        let random_y: f32 = rng.gen_range(-10.0..=30.0);
-
-        Self {
-            position,
-            velocity: Vec2::new(-30.0 + random_x, -80.0 + random_y),
-            acceleration: Vec2::new(0.0, 0.0),
-            ..Default::default()
-        }
-    }
-
-    pub fn step(&mut self, duration: Duration, gravity: Vec2) {
-        let duration = duration.as_secs_f32();
-        self.position += self.velocity * duration + 0.5 * self.acceleration * duration.powi(2);
-        self.velocity += self.acceleration * duration;
-        self.acceleration = gravity;
-    }
-
-    pub fn simulate(
-        &self,
-        gravity: Vec2,
-        delta_time: Duration,
-        sample_every: Duration,
-    ) -> ProjectileSimulator {
-        ProjectileSimulator {
-            projectile: self.clone(),
-            gravity,
-            delta_time,
-            last_sample: Duration::default(),
-            sample_every,
-        }
-    }
-}
-
-struct ProjectileSimulator {
-    projectile: Projectile,
-    gravity: Vec2,
-    delta_time: Duration,
-    last_sample: Duration,
-    sample_every: Duration,
-}
-
-impl Iterator for ProjectileSimulator {
-    type Item = Vec2;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        while self.last_sample < self.sample_every {
-            self.projectile.step(self.delta_time, self.gravity);
-            self.last_sample += self.delta_time;
-        }
-        self.last_sample -= self.sample_every;
-        Some(self.projectile.position)
-    }
+    world_size: Vec2,
+    world_to_screen: Mat3,
 }
 
 impl MainState {
@@ -111,8 +37,27 @@ impl MainState {
             projectile: None,
             projectile_trajectory: None,
             gravity: Vec2::new(0.0, 9.81),
+            world_size: Vec2::new(1000.0, 1000.0),
+            world_to_screen: Mat3::default(),
         };
         Ok(s)
+    }
+
+    fn create_transformation_matrix(&self) -> Mat3 {
+        let screen_width = self.window_size.width as f32;
+        let screen_height = self.window_size.height as f32;
+
+        let scale_x = screen_width / self.world_size.x;
+        let scale_y = screen_height / self.world_size.y;
+
+        // Create a scaling matrix with y-axis flipped
+        let scale_matrix = Mat3::from_scale(Vec2::new(scale_x, -scale_y));
+
+        // Create a translation matrix to move the origin to the bottom-left corner of the screen
+        let translation_matrix = Mat3::from_translation(Vec2::new(0.0, screen_height));
+
+        // Combine the matrices (scaling first, then translation)
+        translation_matrix * scale_matrix
     }
 
     fn render_floor(&self, ctx: &mut Context, canvas: &mut Canvas) -> Result<(), GameError> {
@@ -187,6 +132,10 @@ impl MainState {
             || projectile.y <= radius
             || projectile.y > (self.window_size.height as f32 - self.floor_height + radius))
     }
+
+    fn update_transformations(&mut self) {
+        self.world_to_screen = self.create_transformation_matrix();
+    }
 }
 
 impl event::EventHandler<GameError> for MainState {
@@ -195,6 +144,10 @@ impl event::EventHandler<GameError> for MainState {
             timer::yield_now();
             return Ok(());
         }
+
+        let lol = self.world_to_screen * Vec3::default();
+        let lol2 = self.world_to_screen * Vec3::new(0.0, 100.0, 0.0);
+        let lol23 = self.world_to_screen * Vec3::new(100.0, 100.0, 0.0);
 
         self.opponent_position = Vec2::new(
             (self.window_size.width as f32) * 0.9,
@@ -260,6 +213,7 @@ impl event::EventHandler<GameError> for MainState {
         _height: f32,
     ) -> Result<(), GameError> {
         self.window_size = ctx.gfx.window().inner_size();
+        self.update_transformations();
         Ok(())
     }
 }
@@ -277,4 +231,57 @@ pub fn main() -> GameResult {
     let (mut ctx, event_loop) = cb.build()?;
     let state = MainState::new(&mut ctx)?;
     event::run(ctx, event_loop, state)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn equal_scaling_works() {
+        let mut s = MainState {
+            window_size: PhysicalSize::new(640, 480),
+            floor_height: 100.0,
+            opponent_position: Vec2::default(),
+            projectile: None,
+            projectile_trajectory: None,
+            gravity: Vec2::new(0.0, 9.81),
+            world_size: Vec2::new(640.0, 480.0),
+            world_to_screen: Mat3::default(),
+        };
+
+        s.update_transformations();
+
+        let bottom_left = s.world_to_screen.transform_point2(Vec2::default());
+        let top_of_bottom_left = s.world_to_screen.transform_point2(Vec2::new(0.0, 100.0));
+        let top_right_of_bottom_left = s.world_to_screen.transform_point2(Vec2::new(100.0, 100.));
+
+        assert_eq!(bottom_left, Vec2::new(0.0, 480.0));
+        assert_eq!(top_of_bottom_left, Vec2::new(0.0, 380.0));
+        assert_eq!(top_right_of_bottom_left, Vec2::new(100.0, 380.0));
+    }
+
+    #[test]
+    fn different_scaling_works() {
+        let mut s = MainState {
+            window_size: PhysicalSize::new(640, 480),
+            floor_height: 100.0,
+            opponent_position: Vec2::default(),
+            projectile: None,
+            projectile_trajectory: None,
+            gravity: Vec2::new(0.0, 9.81),
+            world_size: Vec2::new(640.0 * 2.0, 480.0 * 2.0), // twice the size of the screen
+            world_to_screen: Mat3::default(),
+        };
+
+        s.update_transformations();
+
+        let bottom_left = s.world_to_screen.transform_point2(Vec2::default());
+        let top_of_bottom_left = s.world_to_screen.transform_point2(Vec2::new(0.0, 100.0));
+        let top_right_of_bottom_left = s.world_to_screen.transform_point2(Vec2::new(100.0, 100.));
+
+        assert_eq!(bottom_left, Vec2::new(0.0, 480.0));
+        assert_eq!(top_of_bottom_left, Vec2::new(0.0, 480.0 - 50.0));
+        assert_eq!(top_right_of_bottom_left, Vec2::new(50.0, 480.0 - 50.0));
+    }
 }
---
+++ src/projectile.rs
@@ -0,0 +1,82 @@
+use ggez::glam::Vec2;
+use rand::Rng;
+use std::time::Duration;
+
+#[derive(Clone)]
+pub struct Projectile {
+    pub radius: f32, // TODO: This is a rendering concern, not a physical property
+    pub position: Vec2,
+    velocity: Vec2,
+    acceleration: Vec2,
+}
+
+impl Default for Projectile {
+    fn default() -> Self {
+        Self {
+            position: Vec2::default(),
+            velocity: Vec2::default(),
+            acceleration: Vec2::default(),
+            radius: 5.0,
+        }
+    }
+}
+
+impl Projectile {
+    pub fn fire_from(position: Vec2) -> Self {
+        // TODO: Convert from cartesian to polar, angle and force
+
+        let mut rng = rand::thread_rng();
+        let random_x: f32 = rng.gen_range(-10.0..=10.0);
+        let random_y: f32 = rng.gen_range(-10.0..=30.0);
+
+        Self {
+            position,
+            velocity: Vec2::new(-30.0 + random_x, -80.0 + random_y),
+            acceleration: Vec2::new(0.0, 0.0),
+            ..Default::default()
+        }
+    }
+
+    pub fn step(&mut self, duration: Duration, gravity: Vec2) {
+        let duration = duration.as_secs_f32();
+        self.position += self.velocity * duration + 0.5 * self.acceleration * duration.powi(2);
+        self.velocity += self.acceleration * duration;
+        self.acceleration = gravity;
+    }
+
+    pub fn simulate(
+        &self,
+        gravity: Vec2,
+        delta_time: Duration,
+        sample_every: Duration,
+    ) -> ProjectileSimulator {
+        ProjectileSimulator {
+            projectile: self.clone(),
+            gravity,
+            delta_time,
+            last_sample: Duration::default(),
+            sample_every,
+        }
+    }
+}
+
+pub struct ProjectileSimulator {
+    projectile: Projectile,
+    gravity: Vec2,
+    delta_time: Duration,
+    last_sample: Duration,
+    sample_every: Duration,
+}
+
+impl Iterator for ProjectileSimulator {
+    type Item = Vec2;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        while self.last_sample < self.sample_every {
+            self.projectile.step(self.delta_time, self.gravity);
+            self.last_sample += self.delta_time;
+        }
+        self.last_sample -= self.sample_every;
+        Some(self.projectile.position)
+    }
+}
